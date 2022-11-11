e25a280d0520cbd1da19f1f1c64adbb5d6066c84
MathiasWeberComCave <q-mwb@gmx.de>
2022-11-11 09:20:51 +0100

    pap

--- challenge_02.js
+++ challenge_02.js
@@ -16,33 +16,35 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
-// let eingabeName = prompt("Bitte geben Sie den Namen einer Person ein.")
-const eingabeName = "Max";
-let eingabeAlter = Number(prompt("Bitte geben Sie das Alter der Person ein."));
+// // let personName = prompt("Bitte geben Sie den Namen einer Person ein.")
+const personName = "Max";
+let personAlter = Number(prompt("Bitte geben Sie das Alter der Person ein."));
 
-if ( eingabeAlter > 0 && eingabeAlter < 6) {console.log(eingabeName + " trinkt Milch")};
-if ( eingabeAlter >= 6 && eingabeAlter < 13) {console.log(eingabeName + " trinkt Saft")};
-if ( eingabeAlter >= 13 && eingabeAlter < 18) {console.log(eingabeName + " trinkt Cola")};
-if ( eingabeAlter >= 18) {console.log(eingabeName + " trinkt Wein")};
+if ( personAlter > 0 && personAlter < 6) {console.log(personName + " trinkt Milch")};
+if ( personAlter >= 6 && personAlter < 13) {console.log(personName + " trinkt Saft")};
+if ( personAlter >= 13 && personAlter < 18) {console.log(personName + " trinkt Cola")};
+if ( personAlter >= 18) {console.log(personName + " trinkt Wein")};
+
+// die direkte if-anweisung ist doppelt so schnell wie if-else oder switch-case.
 
 
 // switch(true){
 
-//     case (eingabeAlter >0 && eingabeAlter <6):
-//     console.log(eingabeName + " trinkt Milch");
+//     case (personAlter >0 && personAlter <6):
+//     console.log(personName + " trinkt Milch");
 //     break;
-//     case (eingabeAlter >=6 && eingabeAlter <13):
-//     console.log(eingabeName + " trinkt Saft");
+//     case (personAlter >=6 && personAlter <13):
+//     console.log(personName + " trinkt Saft");
 //     break;
-//     case (eingabeAlter >=13 && eingabeAlter <18):
-//     console.log(eingabeName + " trinkt Cola");
+//     case (personAlter >=13 && personAlter <18):
+//     console.log(personName + " trinkt Cola");
 //     break;
-//     case (eingabeAlter >=18):
-//     console.log(eingabeName + " trinkt Wein");
+//     case (personAlter >=18):
+//     console.log(personName + " trinkt Wein");
 //     break;
 
 //     default:
-//     console.log(eingabeName + " trinkt nichts");
+//     console.log(personName + " trinkt nichts");
 //     break;
 // }
    
