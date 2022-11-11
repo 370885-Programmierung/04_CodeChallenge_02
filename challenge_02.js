bd6e5560e10819296098f6e5f3a2bf185cf126a2
MathiasWeberComCave <q-mwb@gmx.de>
2022-11-11 08:45:43 +0100

    if und switchcase

--- challenge_02.js
+++ challenge_02.js
@@ -17,16 +17,32 @@ Alter 13-17 // Cola
 /* Getränke-Challenge */
 
 // let eingabeName = prompt("Bitte geben Sie den Namen einer Person ein.")
-const eingabeName = "Max"
-let eingabeAlter = parseInt(prompt("Bitte geben Sie das Alter der Person ein."))
+const eingabeName = "Max";
+let eingabeAlter = Number(prompt("Bitte geben Sie das Alter der Person ein."));
 
-if (eingabeAlter <6) {
-    console.log(eingabeName + " trinkt Milch")
-} else if (eingabeAlter <13){
-    console.log(eingabeName + " trinkt Saft")
-} else if (eingabeAlter <18){
-    console.log(eingabeName + " trinkt Cola")
-} else if (eingabeAlter >17){
-    console.log(eingabeName + " trinkt Wein")
-} else {console.log(eingabeName + " trinkt nichts")
-}
+if ( eingabeAlter > 0 && eingabeAlter < 6) {console.log(eingabeName + " trinkt Milch")};
+if ( eingabeAlter >= 6 && eingabeAlter < 13) {console.log(eingabeName + " trinkt Saft")};
+if ( eingabeAlter >= 13 && eingabeAlter < 18) {console.log(eingabeName + " trinkt Cola")};
+if ( eingabeAlter >= 18) {console.log(eingabeName + " trinkt Wein")};
+
+
+// switch(true){
+
+//     case (eingabeAlter >0 && eingabeAlter <6):
+//     console.log(eingabeName + " trinkt Milch");
+//     break;
+//     case (eingabeAlter >=6 && eingabeAlter <13):
+//     console.log(eingabeName + " trinkt Saft");
+//     break;
+//     case (eingabeAlter >=13 && eingabeAlter <18):
+//     console.log(eingabeName + " trinkt Cola");
+//     break;
+//     case (eingabeAlter >=18):
+//     console.log(eingabeName + " trinkt Wein");
+//     break;
+
+//     default:
+//     console.log(eingabeName + " trinkt nichts");
+//     break;
+// }
+   
